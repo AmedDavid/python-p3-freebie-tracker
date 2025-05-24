9a125fcbd78964dcf74a8a5323f614f66d04e27d
David Amedi <amukoahdavid@gmail.com>
2025-05-25 01:39:22 +0300

    Added session setup to debug.py for interactive testing

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
+import os
 from sqlalchemy import create_engine
-
-from models import Company, Dev
+from sqlalchemy.orm import sessionmaker
+from models import Company, Dev, Freebie
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///freebies.db')
+    # Use absolute path to ensure correct database location
+    db_path = os.path.join(os.path.dirname(__file__), 'freebies.db')
+    engine = create_engine(f'sqlite:///{db_path}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
     import ipdb; ipdb.set_trace()
